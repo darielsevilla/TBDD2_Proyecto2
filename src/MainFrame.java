12d69137277f805670418a81b4bc4823da6bdec9
Andrea Ortez <andreaortez11@unitec.edu>
2024-06-04 16:16:38 -0600

    menu principal

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,3 +1,6 @@
+
+import java.awt.Color;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -12,8 +15,10 @@ public class MainFrame extends javax.swing.JFrame {
     private Admin admin = null;
     public MainFrame() {
         initComponents();
-        admin = new Admin("Prueba", "AdventureWorks2022", "2606","1433","user1", "prueba1", "Emilio2606", "Emilio2606", 1);
+        this.setLocationRelativeTo(null);
+        this.setResizable(true);
         
+        admin = new Admin("Prueba", "AdventureWorks2022", "2606","1433","user1", "prueba1", "Emilio2606", "Emilio2606", 1);
         admin.connect();
     }
 
@@ -26,21 +31,223 @@ public class MainFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        Fondo = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        pn_BDdestino = new javax.swing.JPanel();
+        jLabel4 = new javax.swing.JLabel();
+        tf_instanciaD = new javax.swing.JTextField();
+        jLabel5 = new javax.swing.JLabel();
+        tf_BDD = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
+        tf_puertoD = new javax.swing.JTextField();
+        jLabel7 = new javax.swing.JLabel();
+        tf_usuarioD = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
+        tf_contraD = new javax.swing.JTextField();
+        bt_probarD = new javax.swing.JPanel();
+        jLabel9 = new javax.swing.JLabel();
+        pn_BDorigen = new javax.swing.JPanel();
+        jLabel10 = new javax.swing.JLabel();
+        tf_instanciaO = new javax.swing.JTextField();
+        jLabel11 = new javax.swing.JLabel();
+        tf_BDO = new javax.swing.JTextField();
+        jLabel12 = new javax.swing.JLabel();
+        tf_puertoO = new javax.swing.JTextField();
+        jLabel13 = new javax.swing.JLabel();
+        tf_usuarioO = new javax.swing.JTextField();
+        jLabel14 = new javax.swing.JLabel();
+        tf_contraO = new javax.swing.JTextField();
+        bt_probarO = new javax.swing.JPanel();
+        jLabel16 = new javax.swing.JLabel();
+        bt_guardar = new javax.swing.JPanel();
+        jLabel15 = new javax.swing.JLabel();
+        jLabel17 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        Fondo.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 20)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Bases de Datos Destino");
+        Fondo.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(461, 90, 410, -1));
+
+        jLabel3.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 20)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Bases de Datos Origen");
+        Fondo.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(32, 90, 410, -1));
+
+        pn_BDdestino.setBackground(new java.awt.Color(245, 247, 251));
+        pn_BDdestino.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(79, 125, 254));
+        jLabel4.setText("Nombre de Instancia");
+        pn_BDdestino.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 30, -1, -1));
+        pn_BDdestino.add(tf_instanciaD, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 30, 170, -1));
+
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(79, 125, 254));
+        jLabel5.setText("Nombre de Base de Datos");
+        pn_BDdestino.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 80, -1, -1));
+        pn_BDdestino.add(tf_BDD, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 80, 170, -1));
+
+        jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(79, 125, 254));
+        jLabel6.setText("Puerto");
+        pn_BDdestino.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 130, -1, -1));
+        pn_BDdestino.add(tf_puertoD, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 130, 170, -1));
+
+        jLabel7.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(79, 125, 254));
+        jLabel7.setText("Nombre de Usuario");
+        pn_BDdestino.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 180, -1, -1));
+        pn_BDdestino.add(tf_usuarioD, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 180, 170, -1));
+
+        jLabel8.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(79, 125, 254));
+        jLabel8.setText("Contraseña");
+        pn_BDdestino.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 230, -1, -1));
+        pn_BDdestino.add(tf_contraD, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 230, 170, -1));
+
+        bt_probarD.setBackground(new java.awt.Color(79, 125, 254));
+        bt_probarD.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                bt_probarDMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                bt_probarDMouseExited(evt);
+            }
+        });
+        bt_probarD.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel9.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel9.setText("Probar");
+        bt_probarD.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, 100, -1));
+
+        pn_BDdestino.add(bt_probarD, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 290, 100, 40));
+
+        Fondo.add(pn_BDdestino, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 130, 410, 360));
+
+        pn_BDorigen.setBackground(new java.awt.Color(245, 247, 251));
+        pn_BDorigen.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel10.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(79, 125, 254));
+        jLabel10.setText("Nombre de Instancia");
+        pn_BDorigen.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 30, -1, -1));
+        pn_BDorigen.add(tf_instanciaO, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 30, 170, -1));
+
+        jLabel11.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel11.setForeground(new java.awt.Color(79, 125, 254));
+        jLabel11.setText("Nombre de Base de Datos");
+        pn_BDorigen.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 80, -1, -1));
+        pn_BDorigen.add(tf_BDO, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 80, 170, -1));
+
+        jLabel12.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel12.setForeground(new java.awt.Color(79, 125, 254));
+        jLabel12.setText("Puerto");
+        pn_BDorigen.add(jLabel12, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 130, -1, -1));
+        pn_BDorigen.add(tf_puertoO, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 130, 170, -1));
+
+        jLabel13.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel13.setForeground(new java.awt.Color(79, 125, 254));
+        jLabel13.setText("Nombre de Usuario");
+        pn_BDorigen.add(jLabel13, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 180, -1, -1));
+        pn_BDorigen.add(tf_usuarioO, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 180, 170, -1));
+
+        jLabel14.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel14.setForeground(new java.awt.Color(79, 125, 254));
+        jLabel14.setText("Contraseña");
+        pn_BDorigen.add(jLabel14, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 230, -1, -1));
+        pn_BDorigen.add(tf_contraO, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 230, 170, -1));
+
+        bt_probarO.setBackground(new java.awt.Color(79, 125, 254));
+        bt_probarO.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                bt_probarOMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                bt_probarOMouseExited(evt);
+            }
+        });
+        bt_probarO.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel16.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel16.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel16.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel16.setText("Probar");
+        bt_probarO.add(jLabel16, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, 100, -1));
+
+        pn_BDorigen.add(bt_probarO, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 290, 100, 40));
+
+        Fondo.add(pn_BDorigen, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 130, 410, 360));
+
+        bt_guardar.setBackground(new java.awt.Color(79, 125, 254));
+        bt_guardar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                bt_guardarMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                bt_guardarMouseExited(evt);
+            }
+        });
+        bt_guardar.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel15.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel15.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel15.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel15.setText("Guardar");
+        bt_guardar.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 10, 100, -1));
+
+        Fondo.add(bt_guardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(400, 530, -1, 40));
+
+        jLabel17.setFont(new java.awt.Font("Segoe UI", 3, 24)); // NOI18N
+        jLabel17.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel17.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel17.setText("CONFIGURACIÓN DE BASES DE DATOS ");
+        Fondo.add(jLabel17, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 30, 900, 30));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Fotos/fondo 4.png"))); // NOI18N
+        Fondo.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 900, -1));
+
+        getContentPane().add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 900, 600));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void bt_probarOMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt_probarOMouseEntered
+        bt_probarO.setBackground(new Color(68,212,229));
+    }//GEN-LAST:event_bt_probarOMouseEntered
+
+    private void bt_probarOMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt_probarOMouseExited
+        bt_probarO.setBackground(new Color(79,125,254));
+    }//GEN-LAST:event_bt_probarOMouseExited
+
+    private void bt_probarDMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt_probarDMouseEntered
+        bt_probarD.setBackground(new Color(68,212,229));
+    }//GEN-LAST:event_bt_probarDMouseEntered
+
+    private void bt_probarDMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt_probarDMouseExited
+        bt_probarD.setBackground(new Color(79,125,254));
+    }//GEN-LAST:event_bt_probarDMouseExited
+
+    private void bt_guardarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt_guardarMouseEntered
+        bt_guardar.setBackground(new Color(195,22,28));
+    }//GEN-LAST:event_bt_guardarMouseEntered
+
+    private void bt_guardarMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_bt_guardarMouseExited
+        bt_guardar.setBackground(new Color(79,125,254));
+    }//GEN-LAST:event_bt_guardarMouseExited
 
     /**
      * @param args the command line arguments
@@ -78,5 +285,38 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel Fondo;
+    private javax.swing.JPanel bt_guardar;
+    private javax.swing.JPanel bt_probarD;
+    private javax.swing.JPanel bt_probarO;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
+    private javax.swing.JLabel jLabel16;
+    private javax.swing.JLabel jLabel17;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JPanel pn_BDdestino;
+    private javax.swing.JPanel pn_BDorigen;
+    private javax.swing.JTextField tf_BDD;
+    private javax.swing.JTextField tf_BDO;
+    private javax.swing.JTextField tf_contraD;
+    private javax.swing.JTextField tf_contraO;
+    private javax.swing.JTextField tf_instanciaD;
+    private javax.swing.JTextField tf_instanciaO;
+    private javax.swing.JTextField tf_puertoD;
+    private javax.swing.JTextField tf_puertoO;
+    private javax.swing.JTextField tf_usuarioD;
+    private javax.swing.JTextField tf_usuarioO;
     // End of variables declaration//GEN-END:variables
 }
